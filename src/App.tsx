a54cbff330542992e9bda63cb5f9bcc3902ebc81
William Mendonça <william@tecmasternet.com.br>
2021-03-25 15:06:19 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react';
+
+import { api } from './services/api';
+
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+
+import './styles/global.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [selectedGenreId]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar
+        genres={genres}
+        handleClickButton={handleClickButton}
+        selectedGenreId={selectedGenreId}
+      />
+      <Content selectedGenre={selectedGenre} movies={movies} />
+    </div>
+  )
+}
---
+++ src/components/Content.tsx
@@ -0,0 +1,40 @@
+import { MovieCard } from './MovieCard';
+
+import '../styles/content.scss';
+
+interface GenreResponseProps {
+  title: string;
+}
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps;
+  movies: MovieProps[]
+}
+
+export function Content({ selectedGenre, movies }: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
+}
